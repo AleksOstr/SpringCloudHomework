dc6508f3ba7ed5ae3f288a08aa6f414d0ebc1e0a
AleksOstr <ostryankoam@yandex.ru>
2024-02-18 17:24:20 +0400

    TimerStarter added

--- reader-service/src/main/java/ru/gb/readerservice/controller/ReaderController.java
+++ reader-service/src/main/java/ru/gb/readerservice/controller/ReaderController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import ru.gb.readerservice.model.ReaderEntity;
 import ru.gb.readerservice.request.ReaderRequest;
 import ru.gb.readerservice.service.ReaderService;
+import ru.gb.timerstarter.Timer;
 
 import java.util.List;
 
@@ -18,18 +19,21 @@ public class ReaderController {
         this.readerService = readerService;
     }
 
+    @Timer
     @GetMapping("/all")
     public ResponseEntity<List<ReaderEntity>> findAll() {
         List<ReaderEntity> readers = readerService.findAll();
         return ResponseEntity.ok(readers);
     }
 
+    @Timer
     @GetMapping("/by-name")
     public ResponseEntity<ReaderEntity> findByName(@RequestBody ReaderRequest request) {
         ReaderEntity reader = readerService.findByName(request);
         return ResponseEntity.ok(reader);
     }
 
+    @Timer
     @PostMapping
     public ResponseEntity<ReaderEntity> save(@RequestBody ReaderRequest request) {
         ReaderEntity reader = readerService.save(request);
