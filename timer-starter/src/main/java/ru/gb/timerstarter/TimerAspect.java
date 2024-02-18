08e96462586dcb0995be3a45f3b94c2b6bac9496
AleksOstr <ostryankoam@yandex.ru>
2024-02-18 17:23:40 +0400

    Correction

--- timer-starter/src/main/java/ru/gb/timerstarter/TimerAspect.java
+++ timer-starter/src/main/java/ru/gb/timerstarter/TimerAspect.java
@@ -14,7 +14,7 @@ import java.time.LocalTime;
 @Slf4j
 public class TimerAspect {
 
-    @Pointcut("@@annotation(ru.gb.timerstarter.Timer)")
+    @Pointcut("@annotation(ru.gb.timerstarter.Timer)")
     public void methodPointcut() {
 
     }
