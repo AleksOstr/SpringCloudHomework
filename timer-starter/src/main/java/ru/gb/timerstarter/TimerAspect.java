c6ca4c3b1cecd95af17144ca4e74f8c278e030ba
AleksOstr <ostryankoam@yandex.ru>
2024-02-18 17:07:18 +0400

    TimerAspect created

---
+++ timer-starter/src/main/java/ru/gb/timerstarter/TimerAspect.java
@@ -0,0 +1,30 @@
+package ru.gb.timerstarter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+@Aspect
+@Slf4j
+public class TimerAspect {
+
+    @Pointcut("@@annotation(ru.gb.timerstarter.Timer)")
+    public void methodPointcut() {
+
+    }
+
+    @Around("methodPointcut()")
+    public Object measureTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long end = System.currentTimeMillis() - start;
+        log.info("Method: {} Execution time: {} msec", joinPoint.getSignature().getName(), end);
+        return result;
+    }
+}
