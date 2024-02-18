f2abb9453b3cefa0a17b15e6f1ac98458cb94325
AleksOstr <ostryankoam@yandex.ru>
2024-02-18 17:27:26 +0400

    TimerStarter add

--- book-sevice/src/main/java/ru/geekbrains/booksevice/controller/BookController.java
+++ book-sevice/src/main/java/ru/geekbrains/booksevice/controller/BookController.java
@@ -2,6 +2,7 @@ package ru.geekbrains.booksevice.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import ru.gb.timerstarter.Timer;
 import ru.geekbrains.booksevice.model.BookEntity;
 import ru.geekbrains.booksevice.request.AuthorRequest;
 import ru.geekbrains.booksevice.request.BookRequest;
@@ -21,24 +22,28 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @Timer
     @GetMapping("/all")
     public ResponseEntity<List<BookEntity>> findAll() {
         List<BookEntity> books = bookService.findAllBooks();
         return ResponseEntity.ok(books);
     }
 
+    @Timer
     @GetMapping("/by-title")
     public ResponseEntity<BookEntity> findByTitle(@RequestBody TitleRequest request) {
         BookEntity book = bookService.findByTitle(request);
         return ResponseEntity.ok(book);
     }
 
+    @Timer
     @GetMapping("/by-author")
     public ResponseEntity<List<BookEntity>> findAllByAuthor(@RequestBody AuthorRequest request) {
         List<BookEntity> books = bookService.findAllByAuthor(request);
         return ResponseEntity.ok(books);
     }
 
+    @Timer
     @PostMapping
     public ResponseEntity<BookEntity> save(@RequestBody BookRequest request) {
         BookEntity newBook = bookService.save(request);
